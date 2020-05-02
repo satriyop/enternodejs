ce9c625ff71103116a8d1ceef49bcec7fb5ed142
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-05-03 01:37:57 +0700

    structuring

---
+++ blog/pages/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Learn Javascript from start]</p>
+        <p>
+          (Stay Tuned !{' '}
+          <a href="https://enterkode.com">Enterkode</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
